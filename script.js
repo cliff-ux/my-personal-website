156ee4b1f004d4902e68a2ee14eaf91122bd05d1
cliff <rydleecliff@gmail.com>
2024-06-23 00:13:48 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const h2= document.createElement("h2");
-h2.textContent = "content added by Java Script";
+h2.textContent = "content  by Java Script";
 document.querySelector("body").appendChild(h2);
+//sticky navbar
+window.addEventListener("scroll" , function(){
+const navbar = this.document.querySelector(' .navbar');
+navbar.classList.toggle('sticky' , window.scrollY> 0);
+}
+);
+// smooth scrolling for anchorlinks
+document.querySelectorAll('a[href^="#"]') .forEach(anchor =>{
+     addEventListener('click' , function (e) ){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')) .scrollintoview({
+            behaviour: 'smooth'
+        });
+
+     };
+});
